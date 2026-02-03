4a08b71f768534c2d89301eba1667bb6e82aa00a
Eliel Delfino <eliel.delfino@fanut.ufal.br>
2026-02-03 10:17:15 -0300

    Ajuste security group

--- ec2.tf
+++ ec2.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "portfolio-guinho" {
 #INGRESS#
 #HTTP
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.lab-sg-guinho.id
+  security_group_id = aws_security_group.portfolio-guinho-ec2.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 80
@@ -57,7 +57,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
 
 #SSH
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.lab-sg-guinho.id
+  security_group_id = aws_security_group.portfolio-guinho-ec2.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
@@ -67,7 +67,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 #HTTPS
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.lab-sg-guinho.id
+  security_group_id = aws_security_group.portfolio-guinho-ec2.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 443
@@ -78,7 +78,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 #SAIDA EC2
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
-  security_group_id = aws_security_group.lab-sg-guinho.id
+  security_group_id = aws_security_group.portfolio-guinho-ec2.id
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = -1
