5bdf73bfb6c904c6b67aef1ffb28c59ed3f7de90
Eliel Delfino <eliel.delfino@fanut.ufal.br>
2026-03-09 20:02:34 -0300

    retorno do antigo terraform do projeto marialazara

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo su
 yum update
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+sudo dpkg -i session-manager-plugin.deb
 yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
